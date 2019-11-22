8bf5cc3a4b28963c9ba9e6afbcfc0ccf995a87a8
Kirwin <pkirwin@bsmschool.org>
2019-11-22 10:13:42 -0600

    custom volume in play sound

--- play_sound.py
+++ play_sound.py
@@ -2,17 +2,17 @@ import sys
 import os
 import time
 
-def play(file = 'AirHorn-SoundBiblecom-964603082.mp3', mute = True, unmute = True):
+def play(file = 'AirHorn-SoundBiblecom-964603082.mp3', mute = True, unmute = True, vol = '100'):
   if mute:
     os.system('amixer sset PCM mute')
-  os.system('omxplayer --vol 100 -o local /home/pi/warning_bell/' + file) 
+  os.system('omxplayer --vol ' + str(vol) + ' -o local /home/pi/warning_bell/' + file) 
   if unmute:
     os.system('amixer sset PCM unmute')
 
 if len(sys.argv) > 1:
   play('requests/door_bell.mp3', True, False)
   time.sleep(1)
-  play('requests/' + sys.argv[1] + '.mp3', False, False)
+  play('requests/' + sys.argv[1] + '.mp3', False, False, 1000)
   time.sleep(1)
-  play('requests/' + sys.argv[1] + '.mp3', False, True)
+  play('requests/' + sys.argv[1] + '.mp3', False, True, 1000)
   time.sleep(1)
